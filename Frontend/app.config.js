f2c318443d1d9931650dadfc68b022688897b317
bara080 <baraahmad232@gmail.com>
2026-07-05 17:22:35 -0400

    making a new build

--- Frontend/app.config.js
+++ Frontend/app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: "Passtime",
     slug: "passtime-app",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/app-icon.png",
     scheme: "passtime",
